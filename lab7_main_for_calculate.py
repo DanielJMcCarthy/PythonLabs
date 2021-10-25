6be0cead808e7f8a7bd98284b3936347555f822c
DanielJMcCarthy <91483917+DanielJMcCarthy@users.noreply.github.com>
2021-10-26 00:15:39 +0100

    main_for_calculate
    Main module which imports calculate module.

---
+++ lab7_main_for_calculate.py
@@ -0,0 +1,16 @@
+"""
+main function to call from lab7_calculate.py
+
+"""
+
+import lab7_calculate
+
+
+
+    
+print(lab7_calculate.sum_of(3, 4, 7, 1, 9))
+print(lab7_calculate.mean(3, 4, 7, 1, 9))
+print(lab7_calculate.maximum(3, 4, 7, 1, 9))
+print(lab7_calculate.minimum(3, 4, 7, 1, 9))
+print(lab7_calculate.max_to_min(3, 4, 7, 1, 9))
+    
